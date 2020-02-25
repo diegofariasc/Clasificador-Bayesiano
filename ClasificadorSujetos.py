65fec3c8e3995653a7d3012d9f8d5a3340891025
Diego Farias Castro <dfc_2014@hotmail.com>
2020-02-24 21:24:59 -0600

    Agregada validacion cruzada

--- ClasificadorSujetos.py
+++ ClasificadorSujetos.py
@@ -34,39 +34,45 @@ def clasificar():
     # Se concluye con una lista con shape (canales x varianzas)
     varianzas_SA = [[ var([sujetoA[canal-1][j][k] for j in range(n_muestras)]) for k in range(n_experimentos) ] for canal in canales]
     varianzas_SB = [[ var([sujetoB[canal-1][j][k] for j in range(n_muestras)]) for k in range(n_experimentos) ] for canal in canales]
-    
-    # Partir el numero de experimentos para el K-Fold mediante division entera
-    n_experimentos //= K
+
+    # Obtener el punto en que se dividira (segun el porcentaje de datos para entrenar)
+    punto_corte = int(n_experimentos * porcentajeEntrenar)
+
+    # Datos clasificados (Calcular cuantos datos se destinan a clasificacion)
+    datosTest = n_experimentos-punto_corte
 
     K_resultados    = []    # Guardar las efectividades de cada iteracion del K-Fold
-    inicioExp       = 0     # Guardar el inicio de la porcion que se considerara en el experimento K
 
     # Iterar K veces
-    for K_experimento in range(K):
+    for K_experimento in range(iteraciones):
+
+        # Mezclar datos 
+        seed(semillaKFold)          # Estandarizar la semilla
+        shuffle(varianzas_SA)       # Ordenar de forma aleatoria los datos del sujeto A
+
+        seed(semillaKFold)          # Estandarizar la semilla
+        shuffle(varianzas_SB)       # Ordenar de forma aleatoria los datos del sujeto B
 
         # Delimitar datos designados a entrenamiento y extraerles media y desviacion
-        # Considerar desde 0 hasta inicio exp y de inicio exp + el numero de experimentos del fold hasta el final
-        #  0 0 0 0 0 0 0  [1 1 1 1 1 1 1] [1 1 1 1 1 1 1]
-        # [1 1 1 1 1 1 1]  0 0 0 0 0 0 0  [1 1 1 1 1 1 1] 
-        # [1 1 1 1 1 1 1] [1 1 1 1 1 1 1]  0 0 0 0 0 0 0 
+        #  0 0 0 0 0 0 0  [1 1 1 1 1 1 1]
 
-        medias_SA     = [mean(canal_varianza[0:inicioExp]+canal_varianza[inicioExp+n_experimentos:]) for canal_varianza in varianzas_SA]   
-        medias_SB     = [mean(canal_varianza[0:inicioExp]+canal_varianza[inicioExp+n_experimentos:]) for canal_varianza in varianzas_SB]
-        desviacion_SA = [std (canal_varianza[0:inicioExp]+canal_varianza[inicioExp+n_experimentos:]) for canal_varianza in varianzas_SA]
-        desviacion_SB = [std (canal_varianza[0:inicioExp]+canal_varianza[inicioExp+n_experimentos:]) for canal_varianza in varianzas_SB]
+        medias_SA     = [mean(canal_varianza[0:punto_corte]) for canal_varianza in varianzas_SA]   
+        medias_SB     = [mean(canal_varianza[0:punto_corte]) for canal_varianza in varianzas_SB]
+        desviacion_SA = [std (canal_varianza[0:punto_corte]) for canal_varianza in varianzas_SA]
+        desviacion_SB = [std (canal_varianza[0:punto_corte]) for canal_varianza in varianzas_SB]
 
         # Fase de clasificacion------------------------------------------------------------------------------
-        # Delimitar datos de clasificacion (test). Solo un fold
+        # Delimitar datos de clasificacion (test)
         
-        varianzas_test_SA = [canal_varianza[inicioExp:inicioExp+n_experimentos] for canal_varianza in varianzas_SA]
-        varianzas_test_SB = [canal_varianza[inicioExp:inicioExp+n_experimentos] for canal_varianza in varianzas_SB]
+        varianzas_test_SA = [canal_varianza[punto_corte:] for canal_varianza in varianzas_SA]
+        varianzas_test_SB = [canal_varianza[punto_corte:] for canal_varianza in varianzas_SB]
 
         # Contadores de clasificados como sujeto A y B y aciertos
         conteo_SA = conteo_SB = aciertos = 0        
 
         # Clasificar experimentos que se sabia eran S1
         # Por cada experimento
-        for exp in range(n_experimentos):
+        for exp in range(datosTest):
 
             # Probabilidad de cada clase
             P_SA = P_SB = 0.5
@@ -87,7 +93,7 @@ def clasificar():
                 conteo_SB +=1
 
         # Clasificar experimentos que se sabia eran S2
-        for exp in range(n_experimentos):
+        for exp in range(datosTest):
             P_SA = P_SB = 0.5
             for canal in range(len(canales)):
                 P_SA *= norm.pdf(varianzas_test_SB[canal-1][exp],medias_SA[canal-1],desviacion_SA[canal-1])
@@ -101,41 +107,28 @@ def clasificar():
                 aciertos+=1
         
         K_resultados.append((conteo_SA,conteo_SB,aciertos))  # Guardar la efectividad K
-        inicioExp = inicioExp + n_experimentos                  # Avanzar porcion de datos considerada en el K-Fold
         
 
-    #print("Clasificados como sujeto A: ",[i[0] for i in K_resultados])
-    #print("Clasificados como sujeto B: ",[i[1] for i in K_resultados])
-    #print("Aciertos:                   ",[i[2] for i in K_resultados])
-    #print("Efectividades:              ",[str(100*i[2]/(2*n_experimentos))+"%" for i in K_resultados])
+    print("Clasificados como sujeto A: ",[i[0] for i in K_resultados])
+    print("Clasificados como sujeto B: ",[i[1] for i in K_resultados])
+    print("Aciertos:                   ",[i[2] for i in K_resultados])
+    print("Efectividades:              ",[str(100*i[2]/(datosTest*2))+"%" for i in K_resultados])
 
-    efectividadesPromedio=100*mean([i[2] for i in K_resultados])/(2*n_experimentos)
-    #print("Efectividad promedio:       ",efectividadesPromedio,"%")
-    return efectividadesPromedio
+    efectividadesPromedio=100*mean([i[2] for i in K_resultados])/(datosTest*2)
+    print("Efectividad promedio:       ",efectividadesPromedio,"%")
 
-for sujeto_A in range(1,9):
-    for sujeto_B in range(sujeto_A+1,10):
-        for clase in range(1,3):
+# Parametros de los archivos
+sujetoA_nombreArchivo = "./Sujetos/S1"   # Nombre de la base de datos del sujetoA
+sujetoB_nombreArchivo = "./Sujetos/S1"    # Nombre de la base de datos del sujetoB
 
-            # Parametros de los archivos
-            sujetoA_nombreArchivo = "./Sujetos/S"+str(sujeto_A)    # Nombre de la base de datos del sujetoA
-            sujetoB_nombreArchivo = "./Sujetos/S"+str(sujeto_B)    # Nombre de la base de datos del sujetoB
+# Parametros de la clasificacion
+canales             = [1,2,3]               # Canales a considerar
+Fs                  = 250                   # Frecuencia de muestreo
+porcentajeEntrenar  = 0.5                   # Porcentaje de los datos que se usaran para entrenar
+iteraciones         = 30                    # Iteraciones a realizar
+semillaKFold        = 1                     # Semilla para mezclar los datos 
+claseUtilizada      = 'C1'                  # Clase a utilizar para distinguir entre sujetos
+bandaInferiorFiltro = 25                    # Banda inferior de frecuencias a filtrar
+bandaSuperiorFiltro = 35                    # Banda superior de frecuencias a filtrar
 
-            efectividades ={}
-            procesados = 0
-
-            for bInf in range (1,49):
-                for bSup in range (bInf,50):
-
-                    # Parametros de la clasificacion
-                    canales             = [1,2,3]               # Canales a considerar
-                    Fs                  = 250                   # Frecuencia de muestreo
-                    K                   = 10                    # Numero K-fold
-                    claseUtilizada      = 'C'+str(clase)        # Clase a utilizar para distinguir entre sujetos
-                    bandaInferiorFiltro = bInf                  # Banda inferior de frecuencias a filtrar
-                    bandaSuperiorFiltro = bSup                  # Banda superior de frecuencias a filtrar
-
-                    efectividades[clasificar()]={"bInf":bInf,"bSup":bSup}
-
-            print("SA:",sujeto_A,"SB:",sujeto_B,"C:",clase,"Efect:",max(efectividades),"Param:",efectividades[max(efectividades)])
-
+clasificar()
